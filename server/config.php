a5092cc285ab8f87062695884150e92b3d46ae0a
Diego Nieto <diego_nieto_1@hotmail.com>
2021-10-22 11:39:42 -0300

    primer commit

---
+++ server/config.php
@@ -0,0 +1,48 @@
+<?php
+namespace Autodesk\ForgeServices;
+use Dotenv\Dotenv;
+
+class ForgeConfig{
+    private static $forge_id = null;
+    private static $forge_secret = null;
+    public static $prepend_bucketkey = true; //toggle client ID prefix to avoid conflict with existing buckets
+
+    public static function getForgeID(){
+      //$forge_id = getenv('FORGE_CLIENT_ID');
+
+      $forge_id = "90Wu7ak6vUw5tqAvk1O1cbXmhs1mmcwb";
+      
+      if(!$forge_id){
+        // load the environment variable from .env into your application
+        $dotenv = Dotenv::create(__DIR__);
+        $dotenv->load();
+        $forge_id = getenv('FORGE_CLIENT_ID');
+      }
+      return $forge_id;
+    }
+
+    public static function getForgeSecret(){
+      //$forge_secret = getenv('FORGE_CLIENT_SECRET');
+      
+      $forge_secret = "ENoqH4GEaezLdjTT";
+      
+      if(!$forge_secret){
+        // load the environment variable from .env into your application
+        $dotenv = Dotenv::create(__DIR__);
+        $dotenv->load();
+        $forge_secret = getenv('FORGE_CLIENT_SECRET');
+     }
+      return $forge_secret;
+    }
+
+    // Required scopes for your application on server-side
+    public static function getScopeInternal(){
+      return ['bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write'];
+    }
+
+    // Required scope of the token sent to the client
+    public static function getScopePublic(){
+      // Will update the scope to viewables:read when #13 of autodesk/forge-client is fixed
+      return ['data:read'];
+    }
+}
